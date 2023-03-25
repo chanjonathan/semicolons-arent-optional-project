22259242f2f265b35752455b7893a56ae7becae4
RubyWEI9393 <wei0903@student.ubc.ca>
2023-03-25 11:28:23 -0700

    add sample pasedTabs list

--- popup.js
+++ popup.js
@@ -15,6 +15,9 @@ const organizeTabs = async () => {
     })
 }
 
+// parsedTabs is a stringified array of objects containing tabId and titles
+// It should look like this:
+// [{"tabId": "abc1", "title": "New Tab"}, {"tabId": "abc2", "title": "GitHub - UBC"}, {"tabId": "abc3", "title": "Outlook"}, {"tabId": "abc4", "title": "Youtube"}, {"tabId": "abc5", "title": "How to become a billionare"}, {"tabId": "abc6", "title": "10 Tips for Cat Lovers"}]
 const prompt = "categorize the tabs into different groups by similarity according to their titles into this format: \n" +
     "```\n" +
     "[{\"label\": \"\", \"tabId\":[\"\", \"\", \"\"...]}, {\"label\": \"\", \"tabId\":[\"\", \"\", \"\"...]}, ....]\n" +
